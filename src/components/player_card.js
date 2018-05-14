ce526d0292821d94685c213b8b43b1862898a9bc
Alex Scarlett <alex.scarlett91@gmail.com>
2018-05-14 15:30:30 -0400

    Folder move

---
+++ src/components/player_card.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class PlayerCard extends Component {
+  click = event => {
+    this.props.handlePlayerClick(this.props.id);
+    this.props.shufflePlayers();
+  };
+
+  render() {
+    return (
+      <div className="card" onClick={this.click}>
+        <div className="img-container">
+          <img alt={this.props.name} src={this.props.image} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default PlayerCard;
